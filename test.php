32830463d3654db31ee1aa1467a6701893117961
Josh Hoyt <josh@janrain.com>
2005-12-20 23:33:30 +0000

    [project @ re-organize test harness]

--- test.php
+++ test.php
@@ -1,29 +1,33 @@
 <?php
 
 require_once('PHPUnit.php');
-require_once('PHPUnit/TestSuite.php');
 require_once('PHPUnit/GUI/HTML.php');
 
-$prefix = 'Tests/OpenID/';
+function loadTests($test_dir, $test_names) {
+	$suites = array();
 
+	foreach ($test_names as $filename) {
+		$filename = $test_dir . $filename . '.php';
+		$class_name = str_replace(DIRECTORY_SEPARATOR, '_', $filename);
+		$class_name = basename($class_name, '.php');
+		include_once($filename);
+		$test = new $class_name($class_name);
+		if (is_a($test, 'PHPUnit_TestCase')) {
+			$test = new PHPUnit_TestSuite($class_name);
+		}
+		$suites[] = $test;
+	}
+
+	return $suites;
+}
+
+$test_dir = 'Tests' . DIRECTORY_SEPARATOR . 'OpenID' . DIRECTORY_SEPARATOR;
 $test_names = array(
 	'KVForm',
 	'CryptUtil',
 	);
 
-$suites = array();
-
-foreach ($test_names as $filename) {
-	$filename = $prefix . $filename . '.php';
-	$className = str_replace(DIRECTORY_SEPARATOR, '_', $filename);
-	$className = basename($className,'.php');
-	include_once($filename);
-	$test = new $className($className);
-	if (is_a($test, 'PHPUnit_TestCase')) {
-		$test = new PHPUnit_TestSuite($className);
-	}
-	$suites[] = $test;
-}
+$suites = loadTests($test_dir, $test_names);
 
 $gui = new PHPUnit_GUI_HTML();
 $gui->addSuites($suites);
