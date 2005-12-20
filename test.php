1f95027905c3db355fdf7ab51459443e3c496397
Josh Hoyt <josh@janrain.com>
2005-12-20 23:28:53 +0000

    [project @ Make test harness smarter about the difference between TestSuites and TestCases]

--- test.php
+++ test.php
@@ -7,22 +7,22 @@ require_once('PHPUnit/GUI/HTML.php');
 $prefix = 'Tests/OpenID/';
 
 $test_names = array(
-	'KVForm'=>FALSE,
-	'CryptUtil'=>TRUE,
+	'KVForm',
+	'CryptUtil',
 	);
 
 $suites = array();
 
-foreach ($test_names as $filename => $introspect) {
+foreach ($test_names as $filename) {
 	$filename = $prefix . $filename . '.php';
 	$className = str_replace(DIRECTORY_SEPARATOR, '_', $filename);
 	$className = basename($className,'.php');
 	include_once($filename);
-	if ($introspect) {
-		$suites[] = new PHPUnit_TestSuite($className);
-	} else {
-		$suites[] = new $className($className);
+	$test = new $className($className);
+	if (is_a($test, 'PHPUnit_TestCase')) {
+		$test = new PHPUnit_TestSuite($className);
 	}
+	$suites[] = $test;
 }
 
 $gui = new PHPUnit_GUI_HTML();
