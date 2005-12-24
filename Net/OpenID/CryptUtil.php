d6c42a8b2fbf58845898fed18e44202d234ec956
Josh Hoyt <josh@janrain.com>
2005-12-24 00:38:31 +0000

    [project @ Add lame entropy source to make getBytes work for platforms where we can't get /dev/urandom]

--- Net/OpenID/CryptUtil.php
+++ Net/OpenID/CryptUtil.php
@@ -1,10 +1,39 @@
 <?php
 
 class Net_OpenID_CryptUtil {
-	function getBytes($num_bytes) {
-		$f = fopen("/dev/urandom", "r");
-		$bytes = fread($f, $num_bytes);
-		fclose($f);
-		return $bytes;
-	}
+    function _getFourBytes() {
+        $x = mt_rand();
+        
+        $sources = array(
+            time(),
+            getmypid(),
+            getmygid(),
+            getmyuid(),
+            disk_free_space(__FILE__)
+            );
+
+        foreach ($sources as $ent) {
+            $x ^= $ent;
+            mt_srand($x);
+            $x = mt_rand();
+        }
+        return $x;
+    }
+
+    function getBytes($num_bytes) {
+        $f = @fopen("/dev/urandom", "r");
+        if ($f === FALSE) {
+            $bytes = '';
+            for ($i = 0; $i < $num_bytes; $i += 4) {
+                $bytes .= Net_OpenID_CryptUtil::_getFourBytes();
+            }
+            $bytes = substr($bytes, 0, $num_bytes);
+        } else {
+            $bytes = fread($f, $num_bytes);
+            fclose($f);
+        }
+        return $bytes;
+    }
 }
+
+?>
