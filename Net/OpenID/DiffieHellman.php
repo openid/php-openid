04e7956ee08e209156f6b55df976cc068d911bf6
tailor <cygnus@janrain.com>
2005-12-28 23:18:03 +0000

    [project @ Added documentation to DiffieHellman.]

--- Net/OpenID/DiffieHellman.php
+++ Net/OpenID/DiffieHellman.php
@@ -1,11 +1,32 @@
 <?php
 
+/**
+ * The OpenID library's Diffie-Hellman implementation.
+ *
+ * PHP versions 4 and 5
+ *
+ * LICENSE: See the COPYING file included in this distribution.
+ *
+ * @package OpenID
+ * @author JanRain, Inc. <openid@janrain.com>
+ * @copyright 2005 Janrain, Inc.
+ * @license http://www.gnu.org/copyleft/lesser.html LGPL
+ */
+
+/**
+ * Check to see if GMP or Bcmath are available and supply the
+ * appropriate implementation.
+ */
 if (extension_loaded('gmp') || @dl('gmp.' . PHP_SHLIB_SUFFIX) ||
     @dl('php_gmp.' . PHP_SHLIB_SUFFIX)) {
 
     define('Net_OpenID_math_type', 'gmp');
 
-    // XXX: untested!
+    /**
+     * The Diffie-Hellman key exchange class.
+     *
+     * @package OpenID
+     */
     class Net_OpenID_DiffieHellman {
         var $DEFAULT_MOD = '155172898181473697471232257763715539915724801966915404479707795314057629378541917580651227423698188993727816152646631438561595825688188889951272158842675419950341258706556549803580104870537681476726513255747040765857479291291572334510643245094715007229621094194349783925984760375594985848253359305585439638443';
 
@@ -55,6 +76,9 @@ if (extension_loaded('gmp') || @dl('gmp.' . PHP_SHLIB_SUFFIX) ||
 } elseif (extension_loaded('bcmath') || @dl('bcmath.' . PHP_SHLIB_SUFFIX) ||
           @dl('php_bcmath.' . PHP_SHLIB_SUFFIX)) {
 
+    /**
+     * @ignore
+     */
     define('Net_OpenID_math_type', 'bcmath');
 
     if (!function_exists('bcpowmod')) {
@@ -77,6 +101,10 @@ if (extension_loaded('gmp') || @dl('gmp.' . PHP_SHLIB_SUFFIX) ||
         }
     }
 
+    /**
+     * @ignore
+     * @package OpenID
+     */
     class Net_OpenID_DiffieHellman {
         var $DEFAULT_MOD = '155172898181473697471232257763715539915724801966915404479707795314057629378541917580651227423698188993727816152646631438561595825688188889951272158842675419950341258706556549803580104870537681476726513255747040765857479291291572334510643245094715007229621094194349783925984760375594985848253359305585439638443';
 
