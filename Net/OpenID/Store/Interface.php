600b43c63ac01c498a9c83b4b36e2814cdcbd494
tailor <cygnus@janrain.com>
2006-01-05 00:13:23 +0000

    [project @ Fixed brace style]

--- Net/OpenID/Store/Interface.php
+++ Net/OpenID/Store/Interface.php
@@ -42,7 +42,8 @@ class Net_OpenID_OpenIDStore {
      *
      * @param Association $association The Association to store.
      */
-    function storeAssociation($server_url, $association) {
+    function storeAssociation($server_url, $association)
+    {
         trigger_error("Net_OpenID_OpenIDStore::storeAssociation ".
                       "not implemented", E_USER_ERROR);
     }
@@ -77,7 +78,8 @@ class Net_OpenID_OpenIDStore {
      * @return Association The Association for the given identity
      * server.
      */
-    function getAssociation($server_url, $handle = null) {
+    function getAssociation($server_url, $handle = null)
+    {
         trigger_error("Net_OpenID_OpenIDStore::getAssociation ".
                       "not implemented", E_USER_ERROR);
     }
@@ -100,7 +102,8 @@ class Net_OpenID_OpenIDStore {
      *
      * @return mixed Returns whether or not the given association existed.
      */
-    function removeAssociation($server_url, $handle) {
+    function removeAssociation($server_url, $handle)
+    {
         trigger_error("Net_OpenID_OpenIDStore::removeAssociation ".
                       "not implemented", E_USER_ERROR);
     }
@@ -113,7 +116,8 @@ class Net_OpenID_OpenIDStore {
      *
      * @return null
      */
-    function storeNonce($nonce) {
+    function storeNonce($nonce)
+    {
         trigger_error("Net_OpenID_OpenIDStore::storeNonce ".
                       "not implemented", E_USER_ERROR);
     }
@@ -133,7 +137,8 @@ class Net_OpenID_OpenIDStore {
      *
      * @return bool Whether or not the nonce was valid.
      */
-    function useNonce($nonce) {
+    function useNonce($nonce)
+    {
         trigger_error("Net_OpenID_OpenIDStore::useNonce ".
                       "not implemented", E_USER_ERROR);
     }
@@ -148,7 +153,8 @@ class Net_OpenID_OpenIDStore {
      * length, and use the full range of byte values. That is, it
      * should be treated as a lump of binary data stored in a string.
      */
-    function getAuthKey() {
+    function getAuthKey()
+    {
         trigger_error("Net_OpenID_OpenIDStore::getAuthKey ".
                       "not implemented", E_USER_ERROR);
     }
@@ -165,7 +171,8 @@ class Net_OpenID_OpenIDStore {
      * @return bool true if the store works fully, false if the
      * consumer will have to use dumb mode to use this store.
      */
-    function isDumb() {
+    function isDumb()
+    {
         return false;
     }
 }
