0b10edb3704cee398f1aa3c992ba773dfcc062b5
Josh Hoyt <josh@janrain.com>
2006-01-06 00:06:47 +0000

    [project @ Fix case of FALSE]

--- Net/OpenID/CryptUtil.php
+++ Net/OpenID/CryptUtil.php
@@ -51,7 +51,7 @@ class Net_OpenID_CryptUtil {
     {
         $bytes = '';
         $f = @fopen("/dev/urandom", "r");
-        if ($f === FALSE) {
+        if ($f === false) {
             if (!defined('Net_OpenID_USE_INSECURE_RAND')) {
                 trigger_error('Set Net_OpenID_USE_INSECURE_RAND to ' .
                               'continue with insecure random.',
@@ -672,7 +672,7 @@ class Net_OpenID_MathLibrary {
         // $Net_OpenID_supported_extensions and try to find an
         // extension that works.
         if (!$Net_OpenID___mathLibrary) {
-            $loaded = FALSE;
+            $loaded = false;
 
             foreach ($Net_OpenID_supported_extensions as $extension) {
                 // See if the extension specified is already loaded.
