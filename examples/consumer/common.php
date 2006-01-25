2ad2158dfba634d9d9e7c3b345682a19f5b7972a
tailor <cygnus@janrain.com>
2006-01-25 23:38:54 +0000

    [project @ Added common.php file for example consumer implementation.]

---
+++ examples/consumer/common.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Require the OpenID consumer code.
+ */
+require_once "Auth/OpenID/Consumer/Consumer.php";
+
+/**
+ * Require the "file store" module, which we'll need to store OpenID
+ * information.
+ */
+require_once "Auth/OpenID/Store/FileStore.php";
+
+/**
+ * This is where the example will store its OpenID information.  You
+ * should change this path if you want the example store to be created
+ * elsewhere.  After you're done playing with the example script,
+ * you'll have to remove this directory manually.
+ */
+$store_path = "/tmp/_php_consumer_test";
+
+if (!file_exists($store_path) &&
+    !mkdir($store_path)) {
+    print "Could not create the FileStore directory '$store_path'. ".
+        " Please check the effective permissions.";
+    exit(0);
+}
+
+$store = new Auth_OpenID_FileStore($store_path);
+
+/**
+ * Create a consumer object using the store object created earlier.
+ */
+$consumer = new Auth_OpenID_Consumer($store);
+
+?>
