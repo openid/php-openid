a2c4ae4d98b72ea0f60cffac5c46fe5b1e7207b7
Josh Hoyt <josh@janrain.com>
2006-01-26 22:26:28 +0000

    [project @ Move test data to its own directory]

--- Tests/Auth/OpenID/Util.php
+++ Tests/Auth/OpenID/Util.php
@@ -7,7 +7,8 @@
 function Tests_Auth_OpenID_readlines($name)
 {
     $path = dirname(realpath(__FILE__));
-    $data_file_name = $path . DIRECTORY_SEPARATOR . $name;
+    $sep = DIRECTORY_SEPARATOR;
+    $data_file_name = $path . $sep . 'data' . $sep . $name;
     $lines = file($data_file_name);
     if ($lines === false) {
         trigger_error("Failed to open data file: $dh_test_data_file",
