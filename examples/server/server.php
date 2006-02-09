997b999147ece7b8c9e2cf3c5cb040196d3564f1
Josh Hoyt <josh@janrain.com>
2006-02-09 00:51:26 +0000

    [project @ Make the entry point script tell the user when he needs to configure the server]

--- examples/server/server.php
+++ examples/server/server.php
@@ -1,18 +1,36 @@
 <?php
 
 require_once 'config.php';
-require_once 'lib/session.php';
-require_once 'lib/actions.php';
 
-init();
+if ($server_url) {
+    require_once 'lib/session.php';
+    require_once 'lib/actions.php';
 
-$action = getAction();
-if (!function_exists($action)) {
-    $action = 'action_default';
+    init();
+
+    $action = getAction();
+    if (!function_exists($action)) {
+        $action = 'action_default';
+    }
+
+    $resp = $action();
+
+    writeResponse($resp);
+} else {
+?>
+<html>
+  <head>
+    <title>PHP OpenID Server</title>
+    <body>
+      <h1>PHP OpenID Server</h1>
+      <p>
+        This server needs to be configured before it can be used. Edit
+        <code>config.php</code> to reflect your server's setup, then
+        load this page again.
+      </p>
+    </body>
+  </head>
+</html>
+<?php
 }
-
-$resp = $action();
-
-writeResponse($resp);
-
 ?>
