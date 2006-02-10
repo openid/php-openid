7b027571294dd9787b8afc57b19e35b27b85a61c
Josh Hoyt <josh@janrain.com>
2006-02-10 22:32:06 +0000

    [project @ Move Consumer.php out of its subdirectory]

--- Tests/Auth/OpenID/Consumer.php
+++ Tests/Auth/OpenID/Consumer.php
@@ -18,7 +18,7 @@ require_once 'Auth/OpenID/DiffieHellman.php';
 require_once 'Auth/OpenID/Store/FileStore.php';
 require_once 'Auth/OpenID/Util.php';
 require_once 'Auth/OpenID/KVForm.php';
-require_once 'Auth/OpenID/Consumer/Consumer.php';
+require_once 'Auth/OpenID/Consumer.php';
 require_once 'PHPUnit.php';
 
 class Auth_OpenID_TestConsumer extends Auth_OpenID_Consumer {
--- examples/consumer/common.php
+++ examples/consumer/common.php
@@ -8,7 +8,7 @@ ini_set('include_path', $path);
 /**
  * Require the OpenID consumer code.
  */
-require_once "Auth/OpenID/Consumer/Consumer.php";
+require_once "Auth/OpenID/Consumer.php";
 
 /**
  * Require the "file store" module, which we'll need to store OpenID
