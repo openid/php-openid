5dac3b5e74664152dc45cd875bd478b0d8821509
tailor <cygnus@janrain.com>
2006-02-13 22:48:46 +0000

    [project @ Added Auth_OpenID class module.]

---
+++ Auth/OpenID.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * This is the PHP OpenID library by JanRain, Inc.
+ *
+ * This module contains core utility functionality used by the
+ * library.  See Consumer.php and Server.php for the consumer and
+ * server implementations.
+ *
+ * PHP versions 4 and 5
+ *
+ * LICENSE: See the COPYING file included in this distribution.
+ *
+ * @package OpenID
+ * @author JanRain, Inc. <openid@janrain.com>
+ * @copyright 2005 Janrain, Inc.
+ * @license http://www.gnu.org/copyleft/lesser.html LGPL
+ */
+
+class Auth_OpenID {
+
+}
+
+?>
