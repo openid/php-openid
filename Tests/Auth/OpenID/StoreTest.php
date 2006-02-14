ecce27920ed37b53efe31d451b21c94ae90919d8
tailor <cygnus@janrain.com>
2006-02-14 23:01:07 +0000

    [project @ Moved MySQL store into its own file.]

--- Tests/Auth/OpenID/StoreTest.php
+++ Tests/Auth/OpenID/StoreTest.php
@@ -472,7 +472,7 @@ explicitly');
             return;
         }
 
-        require_once 'Auth/OpenID/SQLStore.php';
+        require_once 'Auth/OpenID/MySQLStore.php';
         require_once 'DB.php';
 
         global $_Auth_OpenID_db_test_host;
