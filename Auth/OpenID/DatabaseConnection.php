a1cd756a28218b2cebc91834e1efc9040d864f9d
tailor <cygnus@janrain.com>
2006-02-23 01:43:43 +0000

    [project @ Fixed docstring for database connection file.]

--- Auth/OpenID/DatabaseConnection.php
+++ Auth/OpenID/DatabaseConnection.php
@@ -3,12 +3,19 @@
 /**
  * The Auth_OpenID_DatabaseConnection class, which is used to emulate
  * a PEAR database connection.
+ *
+ * @package OpenID
+ * @author JanRain, Inc. <openid@janrain.com>
+ * @copyright 2005 Janrain, Inc.
+ * @license http://www.gnu.org/copyleft/lesser.html LGPL
  */
 
 /**
  * An empty base class intended to emulate PEAR connection
  * functionality in applications that supply their own database
  * abstraction mechanisms.
+ *
+ * @package OpenID
  */
 class Auth_OpenID_DatabaseConnection {
     function setFetchMode($mode)
