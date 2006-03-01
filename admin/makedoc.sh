a39c81061c15cd4666b39c2e84aa4551e3fceaa5
tailor <cygnus@janrain.com>
2006-03-01 00:56:14 +0000

    [project @ Added package-level tutorial file to documentation]

--- admin/makedoc.sh
+++ admin/makedoc.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -v
-phpdoc -p -t doc -d Auth -ti "JanRain OpenID Library" \
+phpdoc -p -t doc -d Auth,tutorials -ti "JanRain OpenID Library" \
     --ignore \*~,BigMath.php,CryptUtil.php,DiffieHellman.php,HMACSHA1.php,KVForm.php,Parse.php,TrustRoot.php \
     -dn "OpenID" -o "HTML:frames:phphtmllib"
