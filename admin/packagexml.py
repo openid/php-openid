212f0d77730557a2428a16a24791cf3c0d297f60
tailor <cygnus@janrain.com>
2006-03-03 19:26:27 +0000

    [project @ Cleaned up line lengths in packagexml.py]

--- admin/packagexml.py
+++ admin/packagexml.py
@@ -17,7 +17,8 @@ def makeLeadXML(leads):
 
 INDENT_STRING = "  "
 
-def buildContentsXMLFordir(dir_or_file, roles, depth=0, dir_role=None, all_files=False):
+def buildContentsXMLFordir(dir_or_file, roles, depth=0, dir_role=None,
+                           all_files=False):
     """
     Returns a list of strings, each of which is either a <file> XML
     element for the given file or a <dir> element which contains other
@@ -29,11 +30,13 @@ def buildContentsXMLFordir(dir_or_file, roles, depth=0, dir_role=None, all_files
         dir_role_s = ''
         if dir_role:
             dir_role_s = ' role="%s"' % (dir_role)
-        lines = ['%s<dir name="%s"%s>' % (INDENT_STRING * depth, os.path.basename(dir_or_file),
+        lines = ['%s<dir name="%s"%s>' % (INDENT_STRING * depth,
+                                          os.path.basename(dir_or_file),
                                           dir_role_s)]
 
         for entry in entries:
-            lines += buildContentsXMLFordir(dir_or_file + os.sep + entry, roles, depth + 1, dir_role, all_files)
+            lines += buildContentsXMLFordir(dir_or_file + os.sep + entry, roles,
+                                            depth + 1, dir_role, all_files)
 
         lines.append('%s</dir>' % (INDENT_STRING * depth))
 
@@ -47,7 +50,8 @@ def buildContentsXMLFordir(dir_or_file, roles, depth=0, dir_role=None, all_files
 
         if extension in roles: # Ends in an extension we care about
             return ['%s<file name="%s" role="%s" />' %
-                    (INDENT_STRING * depth, os.path.basename(dir_or_file), roles[extension])]
+                    (INDENT_STRING * depth, os.path.basename(dir_or_file),
+                     roles[extension])]
         elif all_files:
             return ['%s<file name="%s" />' %
                     (INDENT_STRING * depth, os.path.basename(dir_or_file))]
@@ -67,7 +71,8 @@ def buildDocsXML(*dirs):
     lines = []
 
     for directory in dirs:
-        lines.append("\n".join(buildContentsXMLFordir(directory, {}, 1, 'doc', all_files=True)))
+        lines.append("\n".join(buildContentsXMLFordir(directory, {}, 1, 'doc',
+                                                      all_files=True)))
 
     return "\n".join(lines)
 
@@ -97,14 +102,16 @@ if __name__ == "__main__":
 
     data = xmlconfig.__dict__.copy()
 
-    contents = '<dir name="/">\n' + buildContentsXML({'php': 'php'}, *xmlconfig.contents_dirs) + \
+    contents = '<dir name="/">\n' + \
+               buildContentsXML({'php': 'php'}, *xmlconfig.contents_dirs) + \
                "\n" + buildDocsXML(*xmlconfig.docs_dirs) + '\n  </dir>'
 
     data['contents'] = contents
     data['leads'] = makeLeadXML(xmlconfig.leads)
     data['date'] = time.strftime("%Y-%m-%d")
     data['version'] = version
-    data['uri'] = "%s%s-%s.tgz" % (data['package_base_uri'], data['package_name'], version)
+    data['uri'] = "%s%s-%s.tgz" % (data['package_base_uri'], data['package_name'],
+                                   version)
 
     template_data = template_f.read()
     print template_data % data
