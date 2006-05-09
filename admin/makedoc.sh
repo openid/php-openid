ac98041bd763b3c2cfe8e88f6752c8fd0b88407c
tailor <cygnus@janrain.com>
2006-05-09 22:54:18 +0000

    [project @ Removed discovery code from doc generation]

--- admin/makedoc.sh
+++ admin/makedoc.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -v
 phpdoc -p -t doc -d Auth,admin/tutorials -ti "JanRain OpenID Library" \
-    --ignore \*~,BigMath.php,CryptUtil.php,DiffieHellman.php,HMACSHA1.php,KVForm.php,Parse.php,TrustRoot.php \
+    --ignore \*~,BigMath.php,Discover.php,CryptUtil.php,DiffieHellman.php,HMACSHA1.php,KVForm.php,Parse.php,TrustRoot.php \
     -dn "OpenID" -o "HTML:frames:phphtmllib"
