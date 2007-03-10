338d0a3cc020328d349faff0c5d87a5ff6459f6e
tailor <cygnus@janrain.com>
2007-03-10 00:40:35 +0000

    [project @ Comments in example consumer]

--- examples/consumer/finish_auth.php
+++ examples/consumer/finish_auth.php
@@ -6,16 +6,21 @@ session_start();
 function run() {
     $consumer = getConsumer();
 
-    // Complete the authentication process using the server's response.
+    // Complete the authentication process using the server's
+    // response.
     $response = $consumer->complete($_GET);
 
+    // Check the response status.
     if ($response->status == Auth_OpenID_CANCEL) {
         // This means the authentication was cancelled.
         $msg = 'Verification cancelled.';
     } else if ($response->status == Auth_OpenID_FAILURE) {
+        // Authentication failed; display the error message.
         $msg = "OpenID authentication failed: " . $response->message;
     } else if ($response->status == Auth_OpenID_SUCCESS) {
-        // This means the authentication succeeded.
+        // This means the authentication succeeded; extract the
+        // identity URL and Simple Registration data (if it was
+        // returned).
         $openid = $response->identity_url;
         $esc_identity = htmlspecialchars($openid, ENT_QUOTES);
 
--- examples/consumer/try_auth.php
+++ examples/consumer/try_auth.php
@@ -44,31 +44,40 @@ function run() {
     // Begin the OpenID authentication process.
     $auth_request = $consumer->begin($openid);
 
-    // Handle failure status return values.
+    // No auth request means we can't begin OpenID.
     if (!$auth_request) {
         displayError("Authentication error; not a valid OpenID.");
     }
 
-    $auth_request->addExtensionArg('sreg', 'optional', 'email');
+    // Add an extension arg for optional Simple Registration parameters.
+    $auth_request->addExtensionArg('sreg', 'optional', 'email,postcode');
 
     // Redirect the user to the OpenID server for authentication.
     // Store the token for this authentication so we can verify the
     // response.
 
+    // For OpenID 1, send a redirect.  For OpenID 2, use a Javascript
+    // form to send a POST request to the server.
     if ($auth_request->shouldSendRedirect()) {
         $redirect_url = $auth_request->redirectURL(getTrustRoot(),
                                                    getReturnTo());
 
+        // If the redirect URL can't be built, display an error
+        // message.
         if (Auth_OpenID::isFailure($redirect_url)) {
             displayError("Could not redirect to server: " . $redirect_url->message);
         } else {
+            // Send redirect.
             header("Location: ".$redirect_url);
         }
     } else {
+        // Generate form markup and render it.
         $form_id = 'openid_message';
         $form_html = $auth_request->formMarkup(getTrustRoot(), getReturnTo(),
                                                false, array('id' => $form_id));
 
+        // Display an error if the form markup couldn't be generated;
+        // otherwise, render the HTML.
         if (Auth_OpenID::isFailure($form_html)) {
             displayError("Could not redirect to server: " . $form_html->message);
         } else {
