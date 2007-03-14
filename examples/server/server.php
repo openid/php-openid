cc5a25ff9085fc2fb4941dde70756bbb2fae1ce1
tailor <cygnus@janrain.com>
2007-03-14 23:26:23 +0000

    [project @ Fix include_path in example server server.php]

--- examples/server/server.php
+++ examples/server/server.php
@@ -1,5 +1,10 @@
 <?php
 
+$path_extra = dirname(dirname(dirname(__FILE__)));
+$path = ini_get('include_path');
+$path = $path_extra . PATH_SEPARATOR . $path;
+ini_set('include_path', $path);
+
 $try_include = @include 'config.php';
 
 if (!$try_include) {
