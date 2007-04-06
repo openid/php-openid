abccf2d93c2f1e25518bedd068b7759a03038b6c
tailor <cygnus@janrain.com>
2007-04-06 17:55:20 +0000

    [project @ Use type URI constants in generating XRDS]

--- examples/server/lib/render/idpXrds.php
+++ examples/server/lib/render/idpXrds.php
@@ -3,13 +3,16 @@
 require_once "lib/session.php";
 require_once "lib/render.php";
 
+require_once "Auth/OpenID/Discover.php";
+
 define('xrds_pat', '<?xml version="1.0" encoding="UTF-8"?>
 <xrds:XRDS
     xmlns:xrds="xri://$xrds"
     xmlns="xri://$xrd*($v*2.0)">
   <XRD>
     <Service priority="0">
-      <Type>http://specs.openid.net/auth/2.0/server</Type>
+      <Type>%s</Type>
+      <Type>%s</Type>
       <URI>%s</URI>
     </Service>
   </XRD>
@@ -20,7 +23,10 @@ function idpXrds_render()
 {
     $headers = array('Content-type: application/xrds+xml');
 
-    $body = sprintf(xrds_pat, buildURL());
+    $body = sprintf(xrds_pat,
+                    Auth_OpenID_TYPE_2_0,
+                    Auth_OpenID_TYPE_1_1,
+                    buildURL());
 
     return array($headers, $body);
 }
