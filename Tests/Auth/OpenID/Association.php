b410be8526d0673be852787a482455e57268b0ed
tailor <harningt@gmail.com>
2008-02-02 06:53:07 +0000

    [project @ hmac-sha256-test]

--- Tests/Auth/OpenID/Association.php
+++ Tests/Auth/OpenID/Association.php
@@ -33,6 +33,23 @@ class Tests_Auth_OpenID_Association extends PHPUnit_TestCase {
             $this->assertTrue($assoc2->equal($assoc));
         }
     }
+    function test_me256()
+    {
+        if(!Auth_OpenID_HMACSHA256_SUPPORTED) return;
+        $issued = time();
+        $lifetime = 600;
+        $assoc = new Auth_OpenID_Association('handle', 'secret', $issued,
+                                            $lifetime, 'HMAC-SHA256');
+        $s = $assoc->serialize();
+        $assoc2 = Auth_OpenID_Association::deserialize(
+            'Auth_OpenID_Association', $s);
+
+        if ($assoc2 === null) {
+            $this->fail('deserialize returned null');
+        } else {
+            $this->assertTrue($assoc2->equal($assoc));
+        }
+    }
 }
 
 ?>
