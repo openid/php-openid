e3e8e11b3e268d78772b6adc733a15ac0a73853b
tailor <will@willnorris.com>
2008-06-23 23:15:33 +0000

    [project @ fix accept header]

--- Auth/Yadis/Yadis.php
+++ Auth/Yadis/Yadis.php
@@ -324,7 +324,7 @@ class Auth_Yadis_Yadis {
 
         $request_uri = $uri;
         $headers = array("Accept: " . Auth_Yadis_CONTENT_TYPE .
-                         ', text/html; q=0.3, application/xhtml+xml; 0.5');
+                         ', text/html; q=0.3, application/xhtml+xml; q=0.5');
 
         if ($fetcher === null) {
             $fetcher = Auth_Yadis_Yadis::getHTTPFetcher($timeout);
